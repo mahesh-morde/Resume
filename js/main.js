d6f4ddece2d438c58ac02eb713f5a8739089aa36
Mahi <maheshmorde2511@gmail.com>
2025-07-25 22:54:07 +0530

    minor changes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,3 @@
-// var r = document.querySelector(':root');
-
-// function myFunction_get(propertyName) {
-
-//     var rs = getComputedStyle(r);
-//     return rs.getPropertyValue(propertyName);
-// }
-
-// function myFunction_set_dark() {
-//     var lightButton = document.getElementById('button--light');
-//     var darkButton = document.getElementById('button--dark');
-//     darkButton.setAttribute('hidden', true);
-//     lightButton.removeAttribute('hidden');
-//     r.style.setProperty('--mainTextColor', myFunction_get('--mainTextColor-dark'));
-//     r.style.setProperty('--secondaryTextColor', myFunction_get('--secondaryTextColor-dark'));
-//     r.style.setProperty('--mainLinkColor', myFunction_get('--mainLinkColor-dark'));
-//     r.style.setProperty('--mainBorderColor', myFunction_get('--mainBorderColor-dark'));
-//     r.style.setProperty('--mainBgColor', myFunction_get('--mainBgColor-dark'));
-// }
-
-// function myFunction_set_light() {
-//     var lightButton = document.getElementById('button--light');
-//     var darkButton = document.getElementById('button--dark');
-//     lightButton.setAttribute('hidden', true);
-//     darkButton.removeAttribute('hidden');
-//     r.style.setProperty('--mainTextColor', myFunction_get('--mainTextColor-light'));
-//     r.style.setProperty('--secondaryTextColor', myFunction_get('--secondaryTextColor-light'));
-//     r.style.setProperty('--mainLinkColor', myFunction_get('--mainLinkColor-light'));
-//     r.style.setProperty('--mainBorderColor', myFunction_get('--mainBorderColor-light'));
-//     r.style.setProperty('--mainBgColor', myFunction_get('--mainBgColor-light'));
-// }
-
-// myFunction_set_dark();
-
-
-
 var r = document.querySelector(':root');
 
 function myFunction_get(propertyName) {
@@ -54,6 +18,8 @@ function toggleTheme() {
         r.style.setProperty('--mainLinkColor', myFunction_get('--mainLinkColor-light'));
         r.style.setProperty('--mainBorderColor', myFunction_get('--mainBorderColor-light'));
         r.style.setProperty('--mainBgColor', myFunction_get('--mainBgColor-light'));
+        r.style.setProperty('--cardBgColor', myFunction_get('--cardBgColor-light'));
+        r.style.setProperty('--accentColor', myFunction_get('--accentColor-light'));
     } else {
         lightButton.style.display = 'none';
         darkButton.style.display = 'block';
@@ -62,6 +28,8 @@ function toggleTheme() {
         r.style.setProperty('--mainLinkColor', myFunction_get('--mainLinkColor-dark'));
         r.style.setProperty('--mainBorderColor', myFunction_get('--mainBorderColor-dark'));
         r.style.setProperty('--mainBgColor', myFunction_get('--mainBgColor-dark'));
+        r.style.setProperty('--cardBgColor', myFunction_get('--cardBgColor-dark'));
+        r.style.setProperty('--accentColor', myFunction_get('--accentColor-dark'));
     }
 }
 
