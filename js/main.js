f0679c391202b251325cd4423a4ef45588a8f86a
Mahi <maheshmorde2511@gmail.com>
2026-01-15 00:18:44 +0530

    feat: Redesign resume with glassmorphism UI, updated assets, and enhanced styling.

--- js/main.js
+++ js/main.js
@@ -6,13 +6,9 @@ function myFunction_get(propertyName) {
 }
 
 function toggleTheme() {
-    var lightButton = document.querySelector('.indicator.left');
-    var darkButton = document.querySelector('.indicator.right');
     var toggleSwitch = document.querySelector('.togglesw');
 
     if (toggleSwitch.checked) {
-        lightButton.style.display = 'block';
-        darkButton.style.display = 'none';
         r.style.setProperty('--mainTextColor', myFunction_get('--mainTextColor-light'));
         r.style.setProperty('--secondaryTextColor', myFunction_get('--secondaryTextColor-light'));
         r.style.setProperty('--mainLinkColor', myFunction_get('--mainLinkColor-light'));
@@ -20,9 +16,8 @@ function toggleTheme() {
         r.style.setProperty('--mainBgColor', myFunction_get('--mainBgColor-light'));
         r.style.setProperty('--cardBgColor', myFunction_get('--cardBgColor-light'));
         r.style.setProperty('--accentColor', myFunction_get('--accentColor-light'));
+        r.style.setProperty('--glass-border', myFunction_get('--glass-border-light'));
     } else {
-        lightButton.style.display = 'none';
-        darkButton.style.display = 'block';
         r.style.setProperty('--mainTextColor', myFunction_get('--mainTextColor-dark'));
         r.style.setProperty('--secondaryTextColor', myFunction_get('--secondaryTextColor-dark'));
         r.style.setProperty('--mainLinkColor', myFunction_get('--mainLinkColor-dark'));
@@ -30,6 +25,7 @@ function toggleTheme() {
         r.style.setProperty('--mainBgColor', myFunction_get('--mainBgColor-dark'));
         r.style.setProperty('--cardBgColor', myFunction_get('--cardBgColor-dark'));
         r.style.setProperty('--accentColor', myFunction_get('--accentColor-dark'));
+        r.style.setProperty('--glass-border', myFunction_get('--glass-border-dark'));
     }
 }
 
